ec2798c533f350e8e8fffa38e00f51fbacf0190c
Vijay Bhaskar <kemisettykalyani308@gmail.com>
2023-11-10 12:57:19 +0530

    Network Package created

--- Sources/NetworkServicePackage/NetworkServicePackage.swift
+++ Sources/NetworkServicePackage/NetworkServicePackage.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+public class NetworkService {
+   public func fetch<T: Codable>(url: URL, parse: @escaping (Data) -> T?, completion: @escaping (Result<T?, NetworkError>) -> Void) {
+        
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil, (response as? HTTPURLResponse)?.statusCode == 200 else {
+                completion(.failure(.decodingError))
+                return
+            }
+            let result = parse(data)
+            completion(.success(result))
+        }.resume()
+    }
+}
